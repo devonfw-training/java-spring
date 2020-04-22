197b53668e3e45a8d2359500d0ac0963e897e801
Jörg Hohwiller <hohwille@users.sourceforge.net>
2020-04-22 15:26:24 +0200

    fixed security config

--- demo/core/src/main/java/org/example/demo/general/service/impl/config/WebSecurityBeansConfig.java
+++ demo/core/src/main/java/org/example/demo/general/service/impl/config/WebSecurityBeansConfig.java
@@ -1,5 +1,6 @@
 package org.example.demo.general.service.impl.config;
 
+import org.example.demo.general.logic.impl.config.DefaultRolesPrefixPostProcessor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -7,40 +8,12 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.csrf.CsrfTokenRepository;
 import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
 
-import org.example.demo.general.logic.impl.config.DefaultRolesPrefixPostProcessor;
-import com.devonfw.module.security.common.api.accesscontrol.AccessControlProvider;
-import com.devonfw.module.security.common.base.accesscontrol.AccessControlSchemaProvider;
-import com.devonfw.module.security.common.impl.accesscontrol.AccessControlProviderImpl;
-import com.devonfw.module.security.common.impl.accesscontrol.AccessControlSchemaProviderImpl;
-
 /**
  * This configuration class provides factory methods for several Spring security related beans.
  *
  */
 @Configuration
 public class WebSecurityBeansConfig {
-
-  /**
-   * This method provides a new instance of {@code AccessControlProvider}
-   *
-   * @return the newly created {@code AccessControlProvider}
-   */
-  @Bean
-  public AccessControlProvider accessControlProvider() {
-
-    return new AccessControlProviderImpl();
-  }
-
-  /**
-   * This method provides a new instance of {@code AccessControlSchemaProvider}
-   *
-   * @return the newly created {@code AccessControlSchemaProvider}
-   */
-  @Bean
-  public AccessControlSchemaProvider accessControlSchemaProvider() {
-
-    return new AccessControlSchemaProviderImpl();
-  }
 
   /**
    * This method provides a new instance of {@code CsrfTokenRepository}
