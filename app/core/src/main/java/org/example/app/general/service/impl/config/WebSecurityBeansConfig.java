51c7bea9b563d82a09cce06684afb1ced59e2777
magail <martin.gail@capgemini.com>
2021-05-11 17:17:06 +0200

    0-Setup done

---
+++ app/core/src/main/java/org/example/app/general/service/impl/config/WebSecurityBeansConfig.java
@@ -0,0 +1,52 @@
+package org.example.app.general.service.impl.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
+
+import org.example.app.general.logic.impl.config.DefaultRolesPrefixPostProcessor;
+
+/**
+ * This configuration class provides factory methods for several Spring security related beans.
+ */
+@Configuration
+public class WebSecurityBeansConfig {
+
+  /**
+   * This method provides a new instance of {@code CsrfTokenRepository}
+   *
+   * @return the newly created {@code CsrfTokenRepository}
+   */
+  @Bean
+  public CsrfTokenRepository csrfTokenRepository() {
+
+    return new HttpSessionCsrfTokenRepository();
+  }
+
+  /**
+   * This method provides a new instance of {@code DefaultRolesPrefixPostProcessor}
+   *
+   * @return the newly create {@code DefaultRolesPrefixPostProcessor}
+   */
+  @Bean
+  public static DefaultRolesPrefixPostProcessor defaultRolesPrefixPostProcessor() {
+
+    // By default Spring-Security is setting the prefix "ROLE_" for all permissions/authorities.
+    // We disable this undesired behavior here...
+    return new DefaultRolesPrefixPostProcessor("");
+  }
+
+  /**
+   * This method provide a new instance of {@code DelegatingPasswordEncoder}
+   *
+   * @return the newly create {@code DelegatingPasswordEncoder}
+   */
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+
+    return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+  }
+}
---
+++ app/core/src/test/java/org/example/app/general/common/base/test/ApplicationComponentTest.java
@@ -0,0 +1,22 @@
+package org.example.app.general.common.base.test;
+
+import com.devonfw.module.test.common.base.ComponentTest;
+
+import org.example.app.SpringBootApp;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+
+/**
+ * Abstract base class for {@link ComponentTest}s of this application.
+ */
+@SpringBootTest(classes = { SpringBootApp.class }, webEnvironment = WebEnvironment.MOCK)
+public abstract class ApplicationComponentTest extends ComponentTest {
+
+  @Override
+  protected void doTearDown() {
+    super.doTearDown();
+    TestUtil.logout();
+  }
+
+}
