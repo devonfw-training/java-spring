527de0f5990e0f11d568f6ac11a2e4707beb650d
Jörg Hohwiller <hohwille@users.sourceforge.net>
2021-05-19 14:27:01 +0200

    2 without IFs

--- app/core/src/main/java/org/example/app/ordermanagement/dataaccess/api/ItemEntity.java
+++ app/core/src/main/java/org/example/app/ordermanagement/dataaccess/api/ItemEntity.java
@@ -11,11 +11,10 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import org.example.app.general.dataaccess.api.ApplicationPersistenceEntity;
-import org.example.app.ordermanagement.common.api.Item;
 
 @Entity
 @Table(name = "ITEM")
-public class ItemEntity extends ApplicationPersistenceEntity implements Item {
+public class ItemEntity extends ApplicationPersistenceEntity {
 
   private String name;
 
@@ -26,26 +25,22 @@ public class ItemEntity extends ApplicationPersistenceEntity implements Item {
   private Instant creationDate;
 
   @Column(name = "NAME")
-  @Override
   public String getName() {
 
     return this.name;
   }
 
-  @Override
   public void setName(String name) {
 
     this.name = name;
   }
 
   @Column(name = "PRICE")
-  @Override
   public BigDecimal getPrice() {
 
     return this.price;
   }
 
-  @Override
   public void setPrice(BigDecimal price) {
 
     this.price = price;
@@ -53,26 +48,22 @@ public class ItemEntity extends ApplicationPersistenceEntity implements Item {
 
   @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "ORDER_ID")
-  @Override
   public OrderEntity getOrder() {
 
     return this.order;
   }
 
-  @Override
   public void setOrder(OrderEntity order) {
 
     this.order = order;
   }
 
   @Column(name = "CREATION_DATE")
-  @Override
   public Instant getCreationDate() {
 
     return this.creationDate;
   }
 
-  @Override
   public void setCreationDate(Instant creationDate) {
 
     this.creationDate = creationDate;
